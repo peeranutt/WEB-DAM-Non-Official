09348f4740300a30b69f3c1d56bfeaa00e73bac6
peeranutt <60286536+peeranutt@users.noreply.github.com>
2025-12-08 17:05:54 +0700

    ui upload

---
+++ app/upload/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Navbar from "../../componant/nav";
+import UploadAssets from "../../componant/assets/upload";
+
+export default function Upload() {
+  return (
+    <div>
+      <Navbar />
+      <UploadAssets />
+    </div>
+  );
+}
---
+++ componant/nav.tsx
@@ -0,0 +1,85 @@
+"use client";
+import {
+  Home,
+  Upload,
+  LogIn,
+  FolderClosed,
+  History,
+  Trash2,
+  Server,
+  FileChartLine,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function Navbar() {
+  const router = useRouter();
+  return (
+    <nav className="w-64 h-screen bg-[#F8FAFD] shadow-lg fixed left-0 top-0 flex flex-col">
+      {/* Logo */}
+      <div className="px-6 py-6 border-b">
+        <a href="/" className="text-2xl font-bold text-gray-800">
+          DAM System
+        </a>
+      </div>
+
+      {/* Menu */}
+      <div className="flex-1 flex flex-col py-4 space-y-1">
+        <a
+          href="/"
+          className="flex items-center gap-3 px-6 py-3 text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-sm font-medium"
+        >
+          <Home size={18} />
+          หน้าหลัก
+        </a>
+
+        <button
+          onClick={() => router.push("/upload")}
+          className="w-full flex items-center gap-3 px-6 py-3 text-left text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-sm font-medium"
+        >
+          <Upload size={18} />
+          อัปโหลด
+        </button>
+
+        <a
+          href="/"
+          className="flex items-center gap-3 px-6 py-3 text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-sm font-medium"
+        >
+          <FolderClosed size={18} />
+          คอลเลกชัน
+        </a>
+
+        <a
+          href="/"
+          className="flex items-center gap-3 px-6 py-3 text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-sm font-medium"
+        >
+          <History size={18} />
+          ล่าสุด
+        </a>
+
+        <a
+          href="/"
+          className="flex items-center gap-3 px-6 py-3 text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-sm font-medium"
+        >
+          <Trash2 size={18} />
+          ถังขยะ
+        </a>
+
+        <a
+          href="/"
+          className="flex items-center gap-3 px-6 py-3 text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-sm font-medium"
+        >
+          <Server size={18} />
+          พื้นที่เก็บข้อมูล
+        </a>
+
+        <a
+          href="/"
+          className="flex items-center gap-3 px-6 py-3 text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-sm font-medium"
+        >
+          <FileChartLine size={18} />
+          รายงาน
+        </a>
+      </div>
+    </nav>
+  );
+}
